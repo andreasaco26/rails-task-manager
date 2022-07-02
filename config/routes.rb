6ef711d9c51ec51fa7ea39072508d49a73b95269
andrea saco <andrea.saco26@gmail.com>
2022-07-02 16:06:49 +0200

    finish model

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # read all
+  # read
   get 'tasks', to: 'tasks#index'
-  # read one
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # create get
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # create post
+
+  get 'tasks/new', to: 'tasks#new'
+
+  get 'tasks/:id', to: 'tasks#show', as: "task"
+
+  # creat
   post 'tasks', to: 'tasks#create'
-  # update get
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  # update patch
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
   patch 'tasks/:id', to: 'tasks#update'
+
   # delete
   delete 'tasks/:id', to: 'tasks#destroy'
 end
