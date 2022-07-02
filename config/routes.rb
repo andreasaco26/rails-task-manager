57603b6deffe95f7f6a556a61d5b8472989aa40c
andrea saco <andrea.saco26@gmail.com>
2022-07-02 12:26:53 +0200

    routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # create get
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # create post
+  post 'tasks', to: 'tasks#create'
+  # update get
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  # update patch
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
